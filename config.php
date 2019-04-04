716acae6d9c8f22471f7cdaaede960c2c017e7c2
loveleen <loveleen.nancy@gmail.com>
2019-04-04 21:00:52 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "adarsh");
+
+if(!$con)
+{
+	echo mysqli_error($con);
+}
+?>
---
+++ footer.php
@@ -0,0 +1,49 @@
+          <!-- footer content -->
+       <!--  <footer>
+          <div class="copyright-info">
+            <p class="pull-right">©2019 All Rights Reserved with Pranav Kumar Verma <a href="https://kitkatstudio.com">(Kitkat Studio)</a>    
+            </p>
+          </div>
+          <div class="clearfix"></div>
+        </footer> -->
+        <!-- /footer content -->
+
+      </div>
+      <!-- /page content -->
+    </div>
+
+
+  </div>
+
+
+
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/nicescroll/jquery.nicescroll.min.js"></script>
+
+  <!-- bootstrap progress js -->
+  <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
+  <!-- icheck -->
+  <script src="js/icheck/icheck.min.js"></script>
+  <!-- daterangepicker -->
+  <script type="text/javascript" src="js/moment/moment.min.js"></script>
+  <script type="text/javascript" src="js/datepicker/daterangepicker.js"></script>
+  <!-- chart js -->
+  <script src="js/chartjs/chart.min.js"></script>
+  <!-- sparkline -->
+  <script src="js/sparkline/jquery.sparkline.min.js"></script>
+
+  <script src="js/custom.js"></script>
+
+  <!-- flot js -->
+  <!--[if lte IE 8]><script type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
+  <script type="text/javascript" src="js/flot/jquery.flot.js"></script>
+  <script type="text/javascript" src="js/flot/jquery.flot.pie.js"></script>
+  <script type="text/javascript" src="js/flot/jquery.flot.orderBars.js"></script>
+  <script type="text/javascript" src="js/flot/jquery.flot.time.min.js"></script>
+  <script type="text/javascript" src="js/flot/date.js"></script>
+  <script type="text/javascript" src="js/flot/jquery.flot.spline.js"></script>
+  <script type="text/javascript" src="js/flot/jquery.flot.stack.js"></script>
+  <script type="text/javascript" src="js/flot/curvedLines.js"></script>
+  <script type="text/javascript" src="js/flot/jquery.flot.resize.js"></script>
+  <!-- pace -->
+  <script src="js/pace/pace.min.js"></script>
